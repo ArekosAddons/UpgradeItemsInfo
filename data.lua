d4eb10bbe3957f27284da932aa78fb45b9ef13df
Areko <77594959+WoW-Areko@users.noreply.github.com>
2023-11-11 18:30:10 +0100

    feat: update to new 10.2 tokens

--- data.lua
+++ data.lua
@@ -1,26 +1,31 @@
 local ADDONNAME, ns = ...
 
-local WELPING_MAX_LEVEL = 411
-local DRAKE_MAX_LEVEL = 424
-local WYRM_MAX_LEVEL = 437
-local ASPECT_MAX_LEVEL = 447
 -- [itemID] = maxItemLevel
 local ITEM_TOKENS = {
+    -- -- 10.1, are now grey items
+    -- [204075] = 411, -- Welping fragment
+    -- [204193] = 411, -- Welping crest
 
-    [204075] = WELPING_MAX_LEVEL, -- fragment
-    [204193] = WELPING_MAX_LEVEL, -- crest
+    -- [204076] = 424, -- Drake fragment
+    -- [204195] = 424, -- Drake crest
 
-    [204076] = DRAKE_MAX_LEVEL, -- fragment
-    [204195] = DRAKE_MAX_LEVEL, -- crest
+    -- [204077] = 437, -- Wyrm fragment
+    -- [204196] = 437, -- Wyrm crest
 
-    [204077] = WYRM_MAX_LEVEL, -- fragment
-    [204196] = WYRM_MAX_LEVEL, -- crest
+    -- [204078] = 447, -- Aspect fragment
+    -- [204194] = 447, -- Aspect crest
 }
 
-    [204078] = ASPECT_MAX_LEVEL, -- fragment
-    [204194] = ASPECT_MAX_LEVEL, -- crest
 -- [currencyID] = maxItemLevel
 ns.CURRENCIES_TOKEN = {
+    [2706] = 450, --  Whelpling's Dreaming Crest
+    [2707] = 463, --  Drake's Dreaming Crest
+    [2708] = 476, --  Wyrm's Dreaming Crest
+    [2709] = 489, --  Aspect's Dreaming Crest
+    [2715] = 450, --  Whelpling's Dreaming Crest (No Cap)
+    [2716] = 463, --  Drake's Dreaming Crest (No Cap)
+    [2717] = 476, --  Wyrm's Dreaming Crest (No Cap)
+    [2718] = 489, --  Aspect's Dreaming Crest (No Cap)
 }
 
 ns.ITEM_TOKENS = ITEM_TOKENS
